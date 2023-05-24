62e8b57357689243ebec536479c18529e7b9710e
Sebastian Balzer <sebastian.balzer@gmx.ch>
2023-05-24 13:48:50 +0200

    added preprocessing class

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import hydra
 import tkinter as tk
 import customtkinter as ctk
 from omegaconf import DictConfig
+from loguru import logger
 
-from 
+from preprocessing.preprocessing import PreProcessing
 from filters.nonlocal_means import NonLocalMeansFilter
 
 @hydra.main(version_base=None, config_path='.', config_name='config')
@@ -16,6 +17,11 @@ def main(config: DictConfig) -> None:
     scroll_bar = ctk.CTkScrollbar(master=root, orientation='horizontal')
     scroll_bar.place(relx=0.5, rely=0.8, anchor='center')
 
+    preprocessor = PreProcessing(config)
+    dia, sys, distance_frames = preprocessor()
+
+    nonlocal_means = NonLocalMeansFilter(config)
+    dia_nonlocal_means = nonlocal_means(dia)
 
     if config.filters.plot:
         pass
