37724ea71c863d0e4517cc93380912c1fd715136
Sebastian Balzer <sebastian.balzer@gmx.ch>
2023-06-26 16:57:05 +0200

    updated segmentation (nearly working)

---
+++ segmentation/model.py
@@ -0,0 +1,32 @@
+import pytorch_lightning as pl
+from loguru import logger
+
+
+class Model(pl.LightningModule):
+    def __init__(self, net, criterion, learning_rate, optimizer_class):
+        super().__init__()
+        self.lr = learning_rate
+        self.net = net
+        self.criterion = criterion
+        self.optimizer_class = optimizer_class
+
+    def configure_optimizers(self):
+        optimizer = self.optimizer_class(self.parameters(), lr=self.lr)
+        return optimizer
+
+    def infer_batch(self, batch):
+        x, y = batch
+        y_hat = self.net(x)
+        return y_hat, y
+
+    def training_step(self, batch, batch_idx):
+        y_hat, y = self.infer_batch(batch)
+        loss = self.criterion(y_hat, y)
+        self.log("train_loss", loss, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        y_hat, y = self.infer_batch(batch)
+        loss = self.criterion(y_hat, y)
+        self.log("val_loss", loss, prog_bar=True)
+        return loss
--- segmentation/unet_segmentation.py
+++ segmentation/unet_segmentation.py
@@ -3,79 +3,94 @@ import glob
 import torch
 
 import matplotlib.pyplot as plt
+import pytorch_lightning as pl
 from tqdm import tqdm
+from datetime import datetime
 from loguru import logger
 from monai.losses import DiceCELoss
 from monai.inferers import sliding_window_inference
 from monai.transforms import (
     Compose,
     LoadImage,
+    EnsureChannelFirst,
     RandSpatialCrop,
     ScaleIntensity,
     EnsureType,
     AsDiscrete,
 )
 from monai.metrics import DiceMetric
-from monai.networks.nets import UNETR
+from monai.networks.nets import UNet
 from monai.data import (
     DataLoader,
     CacheDataset,
     ArrayDataset,
 )
 
+from segmentation.model import Model
+
 
 class UNetSegmentation:
     def __init__(self, config) -> None:
         torch.backends.cudnn.benchmark = True
-        self.cuda = torch.cuda.is_available()
+        # self.cuda = torch.cuda.is_available()
+        self.cuda = False
         self.device = torch.device("cuda" if self.cuda else "cpu")
         self.input_shape = (512, 512)
         self.num_classes = 3  # background, lumen, vessel
+
         self.root_dir = config.root_dir
+        self.batch_size = config.segmentation.batch_size
+        self.num_workers = config.segmentation.num_workers
+        self.dropout_rate = config.segmentation.dropout_rate
+        self.learning_rate = config.segmentation.learning_rate
+        self.weight_decay = config.segmentation.weight_decay
+        self.max_epochs = config.segmentation.max_epochs
+
         self.init_transforms()
 
     def __call__(self) -> None:
-        imgs = sorted(glob.glob(os.path.join(self.root_dir, "*img.nii.gz")))
-        segs = sorted(glob.glob(os.path.join(self.root_dir, "*seg.nii.gz")))
+        imgs = sorted(glob.glob(os.path.join(self.root_dir, "*frame_*_img.nii.gz")))
+        segs = sorted(glob.glob(os.path.join(self.root_dir, "*frame_*_seg.nii.gz")))
         dataset = ArrayDataset(imgs, self.img_trafos, segs, self.seg_trafos)
-        train_loader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=8, pin_memory=self.cuda)
+        train_loader = DataLoader(
+            dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers, pin_memory=self.cuda
+        )
 
-        self.model = UNETR(
+        net = UNet(
+            spatial_dims=2,
             in_channels=1,
             out_channels=self.num_classes,
-            img_size=self.input_shape,
-            feature_size=16,
-            hidden_size=768,
-            mlp_dim=3072,
-            num_heads=12,
-            pos_embed="conv",
-            norm_name="instance",
-            res_block=True,
-            dropout_rate=0.0,
-            spatial_dims=2,
-        ).to(self.device)
+            channels=(4, 8, 16),
+            strides=(2, 2),
+            dropout=self.dropout_rate,
+        )
+        loss_function = DiceCELoss(to_onehot_y=True, softmax=True)
+        optimizer = torch.optim.AdamW
+        model = Model(net, loss_function, self.learning_rate, optimizer)
+        early_stopping = pl.callbacks.early_stopping.EarlyStopping(monitor='val_loss')
+        accelerator = 'gpu' if self.cuda else 'cpu'
+        trainer = pl.Trainer(
+            callbacks=[early_stopping], accelerator=accelerator, max_epochs=self.max_epochs, log_every_n_steps=2
+        )
 
-        self.loss_function = DiceCELoss(to_onehot_y=True, softmax=True)
-        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=1e-4, weight_decay=1e-5)
-
+        start = datetime.now()
+        logger.info(f'Training started at {start}')
+        trainer.fit(model=model, train_dataloaders=train_loader)
+        logger.info(f'Training duration: {datetime.now() - start}')
 
     def init_transforms(self) -> None:
         self.img_trafos = Compose(
             [
                 LoadImage(image_only=True),
+                EnsureChannelFirst(),
                 ScaleIntensity(),
-                RandSpatialCrop(self.input_shape, random_size=False),
+                # RandSpatialCrop(self.input_shape, random_size=False),
             ]
         )
         self.seg_trafos = Compose(
             [
                 LoadImage(image_only=True),
-                RandSpatialCrop(self.input_shape, random_size=False),
+                EnsureChannelFirst(),
+                # RandSpatialCrop(self.input_shape, random_size=False),
             ]
         )
-
-    def validation(self):
-        pass
-
-    def train(self):
-        pass
