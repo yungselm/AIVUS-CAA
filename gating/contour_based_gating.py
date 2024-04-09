9ba0dbd052efd7790d3a9cb15ca9087cdcc13685
yungselm_linux <anselm.stark@hotmail.com>
2024-04-09 16:52:51 +0200

    updated gating code

--- gating/contour_based_gating.py
+++ gating/contour_based_gating.py
@@ -18,6 +18,7 @@ class ContourBasedGating:
         self.correlation = None
         self.blurring = None
         self.vertical_lines = []
+        self.selected_line = None
 
     def __call__(self):
         self.main_window.status_bar.showMessage('Contour-based gating...')
@@ -32,8 +33,13 @@ class ContourBasedGating:
         self.vector_angle = self.report_data['vector_angle']
         self.vector_length = self.report_data['vector_length']
         self.define_intramural_part()
+        self.frames = self.crop_frames(x1=50, x2=450, y1=50, y2=450)
+        self.shortest_distance = self.report_data['shortest_distance']
+        self.vector_angle = self.report_data['vector_angle']
+        self.vector_length = self.report_data['vector_length']
         self.prepare_data()
         success, phases = self.plot_data()
+        print(phases)
         if success:
             self.propagate_gating()
             self.update_main_window()
@@ -49,13 +55,14 @@ class ContourBasedGating:
                 lower_limit == 0 and upper_limit == self.main_window.images.shape[0]
             ):  # automatic detection of intramural part
                 mean_elliptic_ratio = self.report_data['elliptic_ratio'].rolling(window=5, closed='both').mean()
+            self.report_data = self.report_data[
+                self.report_data['frame'].between(lower_limit, upper_limit)]
+            self.frames = self.main_window.images[lower_limit:upper_limit]
 
-            self.report_data = self.report_data.iloc[lower_limit:upper_limit, :]
-      
     def crop_frames(self, x1=50, x2=450, y1=50, y2=450):
         """Crops frames to a specific region."""
         cropped_frames = []
-        for frame in self.main_window.images:
+        for frame in self.frames:
             cropped_frame = frame[x1:x2, y1:y2]
             cropped_frames.append(cropped_frame)
         return cropped_frames
@@ -83,10 +90,12 @@ class ContourBasedGating:
 
     def normalize_data(self, data):
         return (data - np.min(data)) / np.sum(data - np.min(data))
-    
+
     def prepare_data(self):
         """Prepares data for plotting."""
-        self.correlation = self.normalize_data(self.calculate_correlation())  # Pass frames as an argument to calculate_correlation()
+        self.correlation = self.normalize_data(
+            self.calculate_correlation()
+        )  # Pass frames as an argument to calculate_correlation()
         self.blurring = self.normalize_data(self.calculate_blurring_fft())
         self.shortest_distance = self.normalize_data(self.shortest_distance)
         self.vector_angle = self.normalize_data(self.vector_angle)
@@ -169,13 +178,12 @@ class ContourBasedGating:
             self.selected_line.set_xdata([event.xdata] * 2)
             plt.draw()
 
-    def get_x_indices():
-        x_indices = [line.get_xdata()[0] for line in vertical_lines]
+    def get_x_indices(self):
+        print(self.vertical_lines)
+        x_indices = [line.get_xdata()[0] for line in self.vertical_lines]
         return x_indices
 
     def plot_data(self):
-        global vertical_lines, selected_line
-
         signal_list_max = [
             self.smooth_curve(self.correlation),
             self.smooth_curve(self.blurring),
@@ -187,26 +195,46 @@ class ContourBasedGating:
             self.smooth_curve(self.vector_length),
         ]
 
-        s_max_w2 = self.combined_signal(signal_list_max, window_size=2, maxima_only=True)
+        # s_max_w2 = self.combined_signal(signal_list_max, window_size=2, maxima_only=True)
         s_max_w5 = self.combined_signal(signal_list_max, window_size=5, maxima_only=True)
-        s_max_w10 = self.combined_signal(signal_list_max, window_size=10, maxima_only=True)
-        s_extrema_w2 = self.combined_signal(singal_list_extrema, window_size=15, maxima_only=False)
+        # s_max_w10 = self.combined_signal(signal_list_max, window_size=10, maxima_only=True)
+        # s_extrema_w2 = self.combined_signal(singal_list_extrema, window_size=15, maxima_only=False)
         s_extrema_w5 = self.combined_signal(singal_list_extrema, window_size=5, maxima_only=False)
-        s_extrema_w10 = self.combined_signal(singal_list_extrema, window_size=10, maxima_only=False)
+        # s_extrema_w10 = self.combined_signal(singal_list_extrema, window_size=10, maxima_only=False)
 
-        vertical_lines = []
-        selected_line = None
+        # check mean difference between s_max and s_extrema curves, and scale smaller curve to match the larger one
+        # for combined signal
+        # mean_max_values = np.mean([s_max_w5, s_max_w10, s_max_w2])
+        # mean_extrema_values = np.mean([s_extrema_w5, s_extrema_w10, s_extrema_w2])
+
+        mean_max_values = np.mean(s_max_w5)
+        mean_extrema_values = np.mean(s_extrema_w5)
+
+        factor_diff = mean_max_values / mean_extrema_values
+
+        if factor_diff < 1:
+            # s_extrema_w2 = s_extrema_w2 * factor_diff
+            s_extrema_w5 = s_extrema_w5 * factor_diff
+            # s_extrema_w10 = s_extrema_w10 * factor_diff
+        else:
+            # s_max_w2 = s_max_w2 * factor_diff
+            s_max_w5 = s_max_w5 * factor_diff
+            # s_max_w10 = s_max_w10 * factor_diff
 
         plt.figure()
 
         # Plot your data
-        plt.plot(s_max_w2, color='r')
+        # plt.plot(s_max_w2, color='r')
         plt.plot(s_max_w5, color='r')
-        plt.plot(s_max_w10, color='r')
-        plt.plot(s_extrema_w2, color='b')
+        # plt.plot(s_max_w10, color='r')
+        # plt.plot(s_extrema_w2, color='b')
         plt.plot(s_extrema_w5, color='b')
-        plt.plot(s_extrema_w10, color='b')
-        plt.legend(['s_max_w2', 's_max_w5', 's_max_w10', 's_extrema_w2', 's_extrema_w5', 's_extrema_w10'])
+        # plt.plot(s_extrema_w10, color='b')
+        # plt.legend(['s_max_w2', 's_max_w5', 's_max_w10', 's_extrema_w2', 's_extrema_w5', 's_extrema_w10'])
+        plt.plot(singal_list_extrema[0], color='grey')
+        plt.plot(singal_list_extrema[1], color='grey')
+        plt.plot(singal_list_extrema[2], color='grey')
+        plt.legend(['s_max_w5', 's_extrema_w5', 'shortest_distance', 'vector_angle', 'vector_length'])
 
         # Connect the event handlers
         plt.connect('button_press_event', self.on_click)
@@ -218,7 +246,6 @@ class ContourBasedGating:
         # Example usage to get x indices
         phases = self.get_x_indices()
         phases = [round(phase, 0) for phase in phases]
-        print(phases)
 
         return True, phases
 
