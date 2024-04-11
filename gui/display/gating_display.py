66c134785e862df055326d655a20c87022faa6c3
Sebastian Balzer <sebastian.balzer@gmx.ch>
2024-04-11 15:51:07 +0200

    Use plt dark mode if system is in dark mode

--- gui/display/gating_display.py
+++ gui/display/gating_display.py
@@ -1,18 +1,22 @@
+import darkdetect
 import matplotlib
 matplotlib.use('Qt5Agg')
+import matplotlib.pyplot as plt
 
 from loguru import logger
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT
-from matplotlib.figure import Figure
 
 
 class GatingDisplay(FigureCanvasQTAgg):
     def __init__(self, main_window, parent=None, width=None, height=None, dpi=100):
+        if darkdetect.isDark():
+            plt.style.use('dark_background')
+
         width = main_window.config.display.image_size if width is None else width
         height = width // 2 if height is None else height
         width /= dpi  # convert pixels to inches
         height /= dpi
-        fig = Figure(figsize=(width, height), dpi=dpi)
+        fig = plt.figure(figsize=(width, height), dpi=dpi)
         super().__init__(fig)
         self.setParent(parent)
         self.toolbar = NavigationToolbar2QT(self, parent)
